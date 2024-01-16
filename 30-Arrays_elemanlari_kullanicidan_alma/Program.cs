c5a22ef142a8d36f90315925b13f0c04514cc032
eryusufnet <yusufer.net@hotmail.com>
2024-01-16 08:41:57 +0200

    Add project files.

---
+++ 30-Arrays_elemanlari_kullanicidan_alma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_Arrays_elemanlari_kullanicidan_alma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // dizinin elemanlarini kullanicidan alma ve yazdirma
+            int[] sayilar = new int[4];
+            for(int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine( );
+            }
+        }
+    }
+}
